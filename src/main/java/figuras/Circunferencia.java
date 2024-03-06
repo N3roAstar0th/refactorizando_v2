198f26ce2c28dc0b275be80e51b68c1480178706
N3roAstar0th <nahuelor.org@gmail.com>
2024-03-06 23:47:14 +0100

    0.9.2. Convertir variable local color en atributo de la clase Circunferencia, ahora se inicializa en el método imprimir.

--- src/main/java/figuras/Circunferencia.java
+++ src/main/java/figuras/Circunferencia.java
@@ -1,6 +1,7 @@
 package figuras;
 public class Circunferencia {
     private double radio;
+    private String color;
 
     public Circunferencia(double radio) {
         this.radio = radio;
@@ -14,7 +15,8 @@ public class Circunferencia {
     }
 
     public void imprimir() {
-        String color = "rojo";
+        this.color = "rojo";
+        String color = this.color;
         System.out.println("Di�metro: " + 2* radio);
         System.out.println("Color: " + color);
         double area = 2 * 3.1416 * radio * radio;
